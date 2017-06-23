0c6ab2da93602365354eeafaa7028d59103c33d3
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-06-23 11:57:11 +0200

    cell class & basic constants

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,4 +1,20 @@
 package constants;
 
 public class Constants {
+
+    private Constants(){
+
+    }
+
+    public static final String APPLICATION_NAME = "John Conway's Game of Life";
+    public static final int NUMBER_OF_ROWS = 30;
+    public static final int NUMBER_OF_COLUMNS = 30;
+    public static final String START = "Start";
+    public static final String RESTART = "Restart";
+    public static final int TIME_LAG = 200;
+
+    public static final int FRAME_WIDTH = 700;
+    public static final int FRAME_HEIGHT = 700;
+
+
 }
--- src/gui/Cell.java
+++ src/gui/Cell.java
@@ -1,4 +1,34 @@
 package gui;
 
-public class Cell {
+import javax.swing.*;
+
+public class Cell extends JPanel {
+
+    private boolean alive = false;
+    private int id;
+
+    public Cell(){
+
+    }
+
+    private void initializeListener(){
+
+    }
+
+    private void initializeLayout(){
+
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
+
+    public void setAlive(boolean alive){
+        this.alive = alive;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell" + id;
+    }
 }
