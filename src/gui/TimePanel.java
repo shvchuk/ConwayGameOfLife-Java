af42402e725417ba02355c6957a89f94d3747d55
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-08-31 22:46:57 +0200

    gui toolbar button listeners and layout

--- src/gui/TimePanel.java
+++ src/gui/TimePanel.java
@@ -1,4 +1,30 @@
 package gui;
 
-public class TimePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class TimePanel extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+    private JLabel timeLabel;
+    public int count = 0;
+
+    public TimePanel(){
+        initializeLayout();
+    }
+
+    private void initializeLayout() {
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        timeLabel = new JLabel("Generations: 0");
+        add(timeLabel);
+    }
+
+    public void refreshCounter(){
+        count = 0;
+        refresh();
+    }
+
+    public void refresh() {
+        timeLabel.setText("Generations: " + count++);
+    }
 }
