0fb34c3e1a208aef5400c71448b9bdbe0367a0d0
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-09-01 23:31:51 +0200

    main frame mainLayout fun

--- src/app/MainFrame.java
+++ src/app/MainFrame.java
@@ -1,4 +1,48 @@
 package app;
 
-public class MainFrame {
+import callbacks.ButtonListener;
+import constants.Constants;
+import gui.Board;
+import gui.TimePanel;
+import gui.Toolbar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.ExecutorService;
+
+public class MainFrame extends JFrame implements ButtonListener{
+
+    private static final long serialVersionUID = 1L;
+    private Board board;
+    private Toolbar toolbar;
+    private TimePanel timePanel;
+    private ExecutorService executor;
+
+    public MainFrame(){
+        super(Constants.APPLICATION_NAME);
+        initializeMenu();
+        initializeMainLayout();
+    }
+
+    private void initializeMainLayout() {
+        toolbar = new Toolbar();
+        timePanel = new TimePanel();
+        board = new Board(timePanel);
+
+        toolbar.setButtonListener(this);
+
+        add(board, BorderLayout.CENTER);
+
+    }
+
+
+    @Override
+    public void startClicked() {
+
+    }
+
+    @Override
+    public void restartClicked() {
+
+    }
 }
